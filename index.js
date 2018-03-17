071e96d099d487b70cdac9978b8c1e410b2b8274
krister <krister@sent.at>
2018-03-17 20:55:27 +0200

    Add back scrict selector checking, but as warnings instead of errors

--- index.js
+++ index.js
@@ -4,5 +4,53 @@ module.exports = {
     "react-native/css-property-no-unknown": true,
     "value-no-vendor-prefix": true,
     "property-no-vendor-prefix": true,
+    "selector-pseudo-class-whitelist": [
+      [],
+      {
+        severity: "warning",
+        message:
+          "pseudo class selectors are ignored by React Native CSS modules. You may still use them for web.",
+      },
+    ],
+    "selector-max-universal": [
+      0,
+      {
+        severity: "warning",
+        message:
+          "universal selectors are ignored by React Native CSS modules. You may still use them for web.",
+      },
+    ],
+    "selector-max-attribute": [
+      0,
+      {
+        severity: "warning",
+        message:
+          "attribute selectors are ignored by React Native CSS modules. You may still use them for web.",
+      },
+    ],
+    "selector-max-type": [
+      0,
+      {
+        severity: "warning",
+        message:
+          "type selectors are ignored by React Native CSS modules. You may still use them for web.",
+      },
+    ],
+    "selector-max-combinators": [
+      0,
+      {
+        severity: "warning",
+        message:
+          "combinator selectors are ignored by React Native CSS modules. You may still use them for web.",
+      },
+    ],
+    "selector-max-id": [
+      0,
+      {
+        severity: "warning",
+        message:
+          "id selectors are ignored by React Native CSS modules. You may still use them for web.",
+      },
+    ],
   },
 };
